19aef8701d9381fc4917a696930062534c140b9b
ItsaLuigiTime <l.shenk1997@gmail.com>
2025-04-10 12:26:17 -0400

    Gradebook
    Added Gradebook table to display grades

---
+++ gradebook.js
@@ -0,0 +1,17 @@
+// TODO: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData() {
+	// this function will query the PostgreSQL database and return grade data
+	console.log("Fetching grade data...");
+}
+
+// TODO: populate the table with grade data
+function populateGradebook(data) {
+	//This function will take the fetched grade data and populate the table
+	console.log("populating gradebook with data:", data);
+}
+
+//TODO remove This
+//Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+// end remove
