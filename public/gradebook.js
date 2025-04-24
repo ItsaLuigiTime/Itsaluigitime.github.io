162200bbc0bfd246a403538f10c6b00039c67d3a
ItsaLuigiTime <l.shenk1997@gmail.com>
2025-04-23 21:23:47 -0400

    testing apiRoute
    testing apiroute

--- public/gradebook.js
+++ public/gradebook.js
@@ -5,7 +5,7 @@ function fetchGradeData() {
     // Create a new request for HTTP data
     let xhr = new XMLHttpRequest();
     // This is the address on the machine we're asking for data
-    let apiRoute = "/api/grades";
+    let apiRoute = "C:\Users\lshen\Documents\GitHub\Itsaluigitime.github.ios\Gradebook_ldshenk";
     // When the request changes status, we run this anonymous function
     xhr.onreadystatechange = function(){
         let results;
